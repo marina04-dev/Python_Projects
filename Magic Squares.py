306c5edd38451198b407aafae48c9484f6b46f4e
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-16 04:10:03 -0700

    Create Magic Squares.py

---
+++ Magic Squares.py
@@ -0,0 +1,13 @@
+# Magic Squares 
+""" A magic square is a 2D matrix composed of n^2 integers
+where n is the length of one row/column. In a magic square
+each row, each column and the 2 diagonals must sum to the 
+same value. """
+
+""" This Function given a 2D matrix the function determimes 
+if the square is magic or not and displays "magic" or "not magic" 
+accordingly """
+
+""" The program takes an input n, representing the length of one 
+row of the matrix. It will then take n lines of input containing
+n integers seperated by spaces. """
