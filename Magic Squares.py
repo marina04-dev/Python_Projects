733fd1bc99f86b3e27b1bd55e4c067f61397844e
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-16 11:14:43 -0700

    Update Magic Squares.py

--- Magic Squares.py
+++ Magic Squares.py
@@ -11,3 +11,41 @@ accordingly """
 """ The program takes an input n, representing the length of one 
 row of the matrix. It will then take n lines of input containing
 n integers seperated by spaces. """
+
+
+""" Magic Squares """
+matrix = []
+n = int(input("Enter the length of one row: "))
+
+# Array Insertion
+for _ in range(n):
+    nums = input("Enter: ")
+    matrix.append([int(x) for x in nums.split()])
+
+# Goal's calculation (summary of the first row)
+magic_sum = sum(matrix[0])
+
+# Checking rows and preparing columns
+columns = [[] for _ in range(n)]
+for row in matrix:
+    if sum(row) != magic_sum:
+        print("Not Magic!")
+        exit()
+    for j, elem in enumerate(row):
+        columns[j].append(elem)
+
+# Columns Checking
+for col in columns:
+    if sum(col) != magic_sum:
+        print("Not Magic!")
+        exit()
+
+# Diagonals Checking
+main_diagonal = sum(matrix[i][i] for i in range(n))
+anti_diagonal = sum(matrix[i][n - 1 - i] for i in range(n))
+
+if main_diagonal != magic_sum or anti_diagonal != magic_sum:
+    print("Not Magic!")
+else:
+    print("Magic!")
+
